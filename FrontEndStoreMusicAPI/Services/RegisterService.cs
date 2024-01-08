5a26df10502c2ab460b4dcb46ffec5217aec29a5
Patryk Prusko <patrykprusko@gmail.com>
2024-01-08 12:17:18 +0100

    added change for details information, when errors are displayed when creating and registering new user

--- FrontEndStoreMusicAPI/Services/RegisterService.cs
+++ FrontEndStoreMusicAPI/Services/RegisterService.cs
@@ -1,5 +1,6 @@
 ﻿using MusicStoreApi.Models;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -41,37 +42,12 @@ namespace FrontEndStoreMusicAPI.Services
                 }
                 else
                 {
+                    int startIndex = responseBody.IndexOf("\"errors\"", StringComparison.OrdinalIgnoreCase);
+                    if (startIndex != -1) responseBody = responseBody.Substring(startIndex);
                     MessageBox.Show("Invalid one of values, Status Code: " + ((int)response.StatusCode) + " -> " + response.StatusCode + "\nDETAILS OF WHAT TO CORRECT: " + responseBody);
                 }
             }
 
-
-
-            //try
-            //{
-
-            //    HttpClient client = new HttpClient();
-            //    client.BaseAddress = new Uri("https://localhost:7195");
-            //    //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //    HttpResponseMessage response = client.PostAsJsonAsync("api/account/register", registerUserDto).Result;
-            //    string responseBody = await response.Content.ReadAsStringAsync();
-            //    responseBody = await response.Content.ReadAsStringAsync();
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        MessageBox.Show("Added new user, " + ((int)response.StatusCode) + ", \n");
-
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("invalid one of values, statusCode: " + ((int)response.StatusCode) + "\n" + responseBody);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
         }
 
     }
